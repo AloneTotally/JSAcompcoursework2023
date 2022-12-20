2a8b5ea507ee42594237e4ee6a21ea489876cbda
AloneTotally <alonzothenoob@gmail.com>
2022-12-21 02:41:19 +0800

    fixed mostly everything

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
 from kivymd.app import MDApp
 from kivy.lang.builder import Builder
-from kivy.uix.widget import Widget
-
-Builder.load_file("homepage.kv")
-
-
-class MyLayout(Widget):
-    pass
-
 
 class HomePage(MDApp):
     def build(self):
-        return MyLayout()
+        return Builder.load_file("homepage.kv")
 
 
-if __name__ == "main":
+if __name__ == '__main__':
     HomePage().run()
