fe1eb4abab4b9f8167e15f9fc3582541061c0f8c
AloneTotally <alonzothenoob@gmail.com>
2022-12-28 18:53:58 +0800

    Added screenmanager and historyitem page

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 from kivymd.app import MDApp
 from kivy.lang.builder import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+
+class HistoryItemScreen(Screen):
+    pass
+
+
+class MainPage(Screen):
+    pass
+
+
+class WindowManager(ScreenManager):
+    pass
 
 
 class HomePage(MDApp):
@@ -7,6 +20,10 @@ class HomePage(MDApp):
         self.theme_cls.primary_palette = "Indigo"
         self.theme_cls.theme_style = "Dark"
         return Builder.load_file("pages/homepage.kv")
+
+    def back_homepage(self):
+        self.root.current = "mainpage"
+        self.root.transition.direction = "right"
 
 
 if __name__ == '__main__':
