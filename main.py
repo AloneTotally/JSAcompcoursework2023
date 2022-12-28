0b788b591e48f4fa476e3beae8cf1b0167d7656a
AloneTotally <alonzothenoob@gmail.com>
2022-12-28 15:01:52 +0800

    tweaked ui

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from kivymd.app import MDApp
 from kivy.lang.builder import Builder
 
+
 class HomePage(MDApp):
     def build(self):
-        return Builder.load_file("homepage.kv")
+        self.theme_cls.primary_palette = "Indigo"
+        self.theme_cls.theme_style = "Dark"
+        return Builder.load_file("pages/homepage.kv")
 
 
 if __name__ == '__main__':
