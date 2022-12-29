b7ba97d3df481515d080d502a0b81b6075608b58
AloneTotally <alonzothenoob@gmail.com>
2022-12-30 03:49:45 +0800

    Added mapview with some basic functionality
    - took way too long
    - is nowhere close to being done

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 from kivymd.app import MDApp
 from kivy.lang.builder import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-
+from kivy_garden.mapview import MapView, MapMarkerPopup, MapMarker, MapSource
+# from kivy.uix.image import AsyncImage
+from kivymd.uix.button import MDRoundFlatButton
 
 class HistoryItemScreen(Screen):
     pass
 
 
 class MainPage(Screen):
-    pass
+
+
+    def map_load(self):
+        print("map_load has been run")
+        self.ids.main_map.center_on(1.3784949677817633, 103.76313504803471)
+        marker = MapMarkerPopup(lat=1.3784949677817633, lon=103.76313504803471)
+        marker.add_widget(MDRoundFlatButton(text="python button", md_bg_color=[0.24705882352941178, 0.3176470588235294, 0.7098039215686275, 1.0], text_color=[1, 1, 1, 1]))
+        self.ids.main_map.add_widget(marker)
 
 
 class WindowManager(ScreenManager):
@@ -16,10 +25,13 @@ class WindowManager(ScreenManager):
 
 
 class HomePage(MDApp):
+
     def build(self):
         self.theme_cls.primary_palette = "Indigo"
         self.theme_cls.theme_style = "Dark"
-        return Builder.load_file("pages/homepage.kv")
+        my_app = Builder.load_file("pages/homepage.kv")
+        return my_app
+
 
     def back_homepage(self):
         self.root.current = "mainpage"
