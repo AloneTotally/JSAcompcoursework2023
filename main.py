76d1d8f8c4e9a17049b229be96a0b970d16c4377
AloneTotally <alonzothenoob@gmail.com>
2023-01-04 23:22:53 +0800

    Added viewlocation page
    - Marker has not been added to the viewlocation page

--- main.py
+++ main.py
@@ -5,18 +5,34 @@ from kivy_garden.mapview import MapView, MapMarkerPopup, MapMarker, MapSource
 # from kivy.uix.image import AsyncImage
 from kivymd.uix.button import MDRoundFlatButton
 
+
 class HistoryItemScreen(Screen):
     pass
 
 
-class MainPage(Screen):
+class ViewLocation(Screen):
+    pass
 
+
+class MainPage(Screen):
+    def view_location(self):
+        print("view_location has been run")
+        self.manager.current = "viewlocation"
+        self.manager.transition.direction = "left"
 
     def map_load(self):
         print("map_load has been run")
         self.ids.main_map.center_on(1.3784949677817633, 103.76313504803471)
         marker = MapMarkerPopup(lat=1.3784949677817633, lon=103.76313504803471)
-        marker.add_widget(MDRoundFlatButton(text="python button", md_bg_color=[0.24705882352941178, 0.3176470588235294, 0.7098039215686275, 1.0], text_color=[1, 1, 1, 1]))
+        marker.add_widget(
+            MDRoundFlatButton(
+                text="python button",
+                md_bg_color=[0.24705882352941178,
+                             0.3176470588235294, 0.7098039215686275, 1.0],
+                text_color=[1, 1, 1, 1],
+                on_press=lambda x: self.view_location()
+            )
+        )
         self.ids.main_map.add_widget(marker)
 
 
@@ -32,6 +48,9 @@ class HomePage(MDApp):
         my_app = Builder.load_file("pages/homepage.kv")
         return my_app
 
+    def to_history_item(self):
+        self.root.current = "historyitem"
+        self.root.transition.direction = "left"
 
     def back_homepage(self):
         self.root.current = "mainpage"
